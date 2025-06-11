d6dac12e146cf2abdb0947a28420f316ea136827
Francesco De Vivo <francescodvf@yahoo.it>
2025-06-11 08:48:37 +0300

    Updated Player.jsx component allowing Name Edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import Player from "./components/Player/Player";
+
 
 function App() {
-  
-
-  return <p className="provisional">Under Construction...</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
+        </ol>
+        GAMEBOARD
+      </div>
+      LOG
+    </main>
+  );
 }
 
 export default App
